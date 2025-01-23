857c01bb21eb4e9d6641888d51375f557c9c3b52
wangy122 <wangy122@chinatelecom.cn>
2025-01-23 11:54:40 +0800

    add user page

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,9 +1,22 @@
 // user.js
 Page({
   data: {
-    
+    userInfo: null
   },
+
   onLoad() {
-    
+    // 尝试获取缓存中的用户信息
+    const userInfo = wx.getStorageSync('userInfo')
+    if (userInfo) {
+      this.setData({ userInfo })
+    }
+  },
+
+  onShow() {
+    // 每次显示页面时更新用户信息
+    const userInfo = wx.getStorageSync('userInfo')
+    if (userInfo) {
+      this.setData({ userInfo })
+    }
   }
 }) 
